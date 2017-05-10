4a8fd58e85a30352bea0d32af11a5924806c10e5
Dingchang Yang <i.septentrion@gmail.com>
2017-05-10 17:49:20 -0400

    Update Solution

--- ProjectEuler+/Project Euler #007/Solution.java
+++ ProjectEuler+/Project Euler #007/Solution.java
@@ -4,54 +4,53 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution {
+public class SolutionAlternative {
 
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     int t = in.nextInt();
+    int[] arrPrime = nthPrime();
     
     for(int a0 = 0; a0 < t; a0++){
       int n = in.nextInt();
-      System.out.println(nthPrime(n));
+      System.out.println(arrPrime[n - 1]);
     }
   }
   
-  // calculate the n-th prime number
-  private static int nthPrime(int n) {
-    if (n == 1) {
-      return 2;
-    }
+  // create an array of the first 10000 primes
+  private static int[] nthPrime() {
+    int[] result = new int[10000];
+    result[0] = 2;
+    int count = 1;
     
-    int result = 3;
-    for (int i = 1; i < n; result += 2) {
-      if (isPrime(result)) {
-        i++;
+    for (int i = 3; count < 10000; i += 2) {
+      if (isPrime(i)) {
+        result[count] = i;
+        count++;
       }
     }
     
-    return result - 2;
+    return result;
   }
   
   // determine if a number is a prime number
   private static boolean isPrime(int n) {
-    if (n == 1)
-      return false;
-    else if (n < 4)
+    if (n == 2) {
       return true;
-    else if (n % 2 == 0)
+    }
+    
+    if (n % 2 == 0) {
       return false;
-    else if (n < 9)
-      return true;
-    else if (n % 3 == 0)
-      return false;
-    else {
-      long limit = (long) Math.floor(Math.sqrt(n));
-
-      for (int i = 5; i <= limit; i += 6) {
-        if (n % i == 0 || n % (i + 2) == 0) 
-          return false;
-      }
-    }   
+    }
+    
+    int limit = (int) Math.floor(Math.sqrt(n));
+      
+    for (int i = 3; i <= limit; i += 2) 
+    {
+      if (n % i == 0) 
+        return false;
+    }
+    
     return true;
   }
 }
